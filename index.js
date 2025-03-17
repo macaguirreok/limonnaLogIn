0695f6c8cdc237bff7ec96cc583a9f761d093137
macaguirreok <141680157+macaguirreok@users.noreply.github.com>
2025-03-16 22:24:17 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ formIngreso.setAttribute("class", "container");
 //Estilos de bootstrap para el formulario:
 formIngreso.classList.add("border");
 formIngreso.classList.add("p-3");
-formIngreso.classList.add("w-50");
+formIngreso.classList.add("w-100");
 
 // Hacer que el formulario se organice en una columna vertical
 formIngreso.classList.add("d-flex");
